7010a020cb3001c96ee80829cef6472cce022fc3
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-02 17:38:18 +0100

    filter done and works!

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 new Vue({
     el: '#root',
     data: {
-        listDisks:[],  
+        listDisks:[],
+        filterValue:'',  
     },
     mounted() {
       const self = this;
         axios.get("http://localhost/php-ajax-dischi/app/server.php")
         .then(function(response) {
-            response.data.forEach(element => {
-                self.listDisks.push(element);
-            });
+            self.listDisks = response.data;
             console.log(self.listDisks);
         });
     },
     methods:{
-      
+      changeList:function(){
+        const self = this;
+        console.log(self.filterValue);
+        axios.get("http://localhost/php-ajax-dischi/app/server.php?genere="+ self.filterValue)
+        .then(function(response) {
+            self.listDisks = response.data;
+            console.log(self.listDisks);
+        });
+      }
     },
   });
   Vue.config.devtools = true
