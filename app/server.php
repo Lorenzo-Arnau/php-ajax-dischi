244c4aa8de8c880fe3e1b793db663f4a010285cd
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-02 15:27:56 +0100

    first commit, ready to start

---
+++ app/server.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../database/database.php';
+print_r($dischi);
+// 2nd milestone:
+// farà il json encode del mio database. => require_once
+// ricordatevi dell'header (vedi le slide)
---
+++ database/notes.md
@@ -0,0 +1,3 @@
+# Qui dentro metteremo i nostri file php che contengono dei dati.
+
+il *database* è da intendersi solo per lo stoccaggio dei data.
