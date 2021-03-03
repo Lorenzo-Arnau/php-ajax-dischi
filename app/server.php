589abfbbe31ca7c2675a59993428fb835a253e7a
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-03 11:25:49 +0100

    update

--- app/server.php
+++ app/server.php
@@ -3,14 +3,13 @@ require_once __DIR__ . '/../database/database.php';
 // 2nd milestone:
 // farà il json encode del mio database. => require_once
 // ricordatevi dell'header (vedi le slide)
-$querySearch = $_GET['genere'];
-if ($querySearch == false) {
-   $dischi = $dischi;
-}else{
-  $dischi = array_filter($dischi, function ($keyValue) use ($querySearch) {
-        return ($keyValue['genre'] == $querySearch);
-    });
+$querySearch = strtolower($_GET['genere']);
+if (!empty($querySearch)) {
+   $dischi = array_filter($dischi, function ($keyValue) use ($querySearch) {
+         return (strpos(strtolower($keyValue['genre']),$querySearch)!== false);
+     });
 }
+
 header('Content-Type: application/json');
 $jason = json_encode($dischi);
 echo $jason;
