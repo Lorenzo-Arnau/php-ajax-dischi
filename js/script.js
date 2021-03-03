2842ed4c85e63eee9df6f598f82b229b5ba18215
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-03 13:32:31 +0100

    update path get request

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ new Vue({
     },
     mounted() {
       const self = this;
-        axios.get("http://localhost/php-ajax-dischi/app/server.php")
+        axios.get("app/server.php")
         .then(function(response) {
             self.listDisks = response.data;
             console.log(self.listDisks);
@@ -18,13 +18,13 @@ new Vue({
         const self = this;
         console.log(self.filterValue);
         if (self.filterValue === 'All') {
-          axios.get("http://localhost/php-ajax-dischi/app/server.php")
+          axios.get('app/server.php')
           .then(function(response) {
               self.listDisks = response.data;
               console.log(self.listDisks);
           });
         }else{
-        axios.get("http://localhost/php-ajax-dischi/app/server.php?genere="+ self.filterValue)
+        axios.get('app/server.php?genere='+ self.filterValue)
         .then(function(response) {
             self.listDisks = response.data;
             console.log(self.listDisks);
