e59da11ba877ae124749fc10f3ef429808407acd
Lorenzo-Arnau <skyline03@hotmail.it>
2021-03-03 12:00:21 +0100

    add a input search

--- app/server.php
+++ app/server.php
@@ -3,12 +3,12 @@ require_once __DIR__ . '/../database/database.php';
 // 2nd milestone:
 // farà il json encode del mio database. => require_once
 // ricordatevi dell'header (vedi le slide)
-$querySearch = strtolower($_GET['genere']);
-if (!empty($querySearch)) {
+if (array_key_exists ( 'genere' , $_GET ) && !empty($_GET['genere'])) {
+   $querySearch = strtolower($_GET['genere']);
    $dischi = array_filter($dischi, function ($keyValue) use ($querySearch) {
          return (strpos(strtolower($keyValue['genre']),$querySearch)!== false);
      });
-}
+    }
 
 header('Content-Type: application/json');
 $jason = json_encode($dischi);
